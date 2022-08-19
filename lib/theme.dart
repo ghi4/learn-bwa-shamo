c79fde398d967dd2b7141fdc9b52fe9511282562
Dhimas Fajarw <ghidet4pcoop@gmail.com>
2022-08-20 03:27:09 +0700

    Add backgroundColor 6 and move 'Another' segment Color to top

--- lib/theme.dart
+++ lib/theme.dart
@@ -8,6 +8,8 @@ Color secondaryColor = const Color(0xff38ABBE);
 Color thirdColor = const Color(0xff808191);
 Color alertColor = const Color(0xffED6363);
 Color priceColor = const Color(0xff2C96F1);
+Color transparentColor = Colors.transparent;
+Color blackColor = const Color(0xff2E2E2E);
 
 // Background Color
 Color backgroundColor1 = const Color(0xff1F1D2B);
@@ -15,15 +17,12 @@ Color backgroundColor2 = const Color(0xff2B2937);
 Color backgroundColor3 = const Color(0xff242231);
 Color backgroundColor4 = const Color(0xff252836);
 Color backgroundColor5 = const Color(0xff2B2844);
+Color backgroundColor6 = const Color(0xffECEDEF);
 
 // Text Color
 Color primaryTextColor = const Color(0xffF1F0F2);
 Color secondaryTextColor = const Color(0xff999999);
 Color subtitleColor = const Color(0xff504F5E);
-
-// Another
-Color transparentColor = Colors.transparent;
-Color blackColor = const Color(0xff2E2E2E);
 
 TextStyle primaryTextStyle = GoogleFonts.poppins(
   color: primaryTextColor,
