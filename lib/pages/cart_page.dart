04452458805c94a23d0fa0d8fc7a367004b1b8ed
Dhimas Fajarw <ghidet4pcoop@gmail.com>
2022-09-09 10:39:12 +0700

    Add Cart Page

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,10 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:shamo/theme.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    header() {
+      return AppBar(
+        backgroundColor: backgroundColor1,
+        centerTitle: true,
+        elevation: 0,
+        title: const Text(
+          'Your Cart',
+        ),
+      );
+    }
+
+    Widget emptyCart() {
+      return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/icon_cart3.png',
+              width: 80,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Opss! Your Cart is Empty',
+              style:
+                  primaryTextStyle.copyWith(fontSize: 16, fontWeight: medium),
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            Text(
+              'Let\'s find your favorite shoes',
+              style: subtitleTextStyle,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.pushNamedAndRemoveUntil(
+                    context, '/home', (route) => false);
+              },
+              style: TextButton.styleFrom(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 24,
+                ),
+                backgroundColor: primaryColor,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+              child: Text(
+                'Explore Store',
+                style:
+                    primaryTextStyle.copyWith(fontSize: 16, fontWeight: medium),
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: backgroundColor2,
+      appBar: header(),
+      body: emptyCart(),
+    );
   }
 }
