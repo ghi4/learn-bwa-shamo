59e9a317440f6a1a25813518f5df8b2009da429b
Dhimas Fajarw <ghidet4pcoop@gmail.com>
2022-10-06 17:08:21 +0700

    Add checkout page

---
+++ lib/pages/checkout_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:shamo/theme.dart';
+
+class CheckoutPage extends StatelessWidget {
+  const CheckoutPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backgroundColor3,
+    );
+  }
+}
